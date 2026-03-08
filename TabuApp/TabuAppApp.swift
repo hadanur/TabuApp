f32782ccf6a10ec7c5067836b58eda78c8f5e20f
hadanur <115171695+hadanur@users.noreply.github.com>
2026-03-08 20:05:07 +0300

    Initial Commit

---
+++ TabuApp/TabuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabuAppApp.swift
+//  TabuApp
+//
+//  Created by Hakan Adanur on 08/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TabuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
